705e86420b678cd866be4aa60b70a786415aa67d
Armend Berisha <Armend.Berisha96@outlook.com>
2025-01-28 23:29:43 +0100

    Add project files.

---
+++ TicketBookingCore.Tests/TicketBookingRequestProcessorTests.cs
@@ -0,0 +1,29 @@
+﻿namespace TicketBookingCore.Tests;
+
+public class TicketBookingRequestProcessorTests
+{
+
+    [Fact]
+    public void ShouldReturnTicketBookingResultWithRequestValues()
+    {
+        // Arrange
+        var processor = new TicketBookingRequestProcessor();
+
+        var request = new TicketBookingRequest
+        {
+            FirstName = "Armend",
+            LastName = "Berisha",
+            Email = "Armend@outlook.com"
+        };
+
+        // Act
+
+        TicketBookingResponse response = processor.Book(request);
+
+        // Assert
+        Assert.NotNull(response);
+        Assert.Equal(request.FirstName, response.FirstName);
+        Assert.Equal(request.LastName, response.LastName);
+        Assert.Equal(request.Email, response.Email);
+    }
+}
